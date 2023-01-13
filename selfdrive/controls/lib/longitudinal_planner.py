248bb704b374d374686dbd7999560b4f2ed28362
amah80 <68617087+amah80@users.noreply.github.com>
2023-01-12 23:56:47 -0800

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -20,7 +20,7 @@ from selfdrive.controls.lib.turn_speed_controller import TurnSpeedController
 from selfdrive.controls.lib.events import Events
 
 LON_MPC_STEP = 0.2  # first step is 0.2s
-AWARENESS_DECEL = -0.2  # car smoothly decel at .2m/s^2 when user is distracted
+AWARENESS_DECEL = -0.01  # car smoothly decel at .2m/s^2 when user is distracted
 A_CRUISE_MIN = -1.2
 A_CRUISE_MAX_VALS = [1.6, 1.2, 0.8, 0.6]
 A_CRUISE_MAX_BP = [0., 10.0, 25., 40.]
@@ -41,14 +41,14 @@ DP_ACCEL_NORMAL = 1
 DP_ACCEL_SPORT = 2
 
 # accel profile by @arne182 modified by cgw
-_DP_CRUISE_MIN_V =       [-0.65,  -0.60,  -0.73, -0.75,  -0.75, -0.75]
-_DP_CRUISE_MIN_V_ECO =   [-0.65,  -0.60,  -0.70, -0.70,  -0.65, -0.65]
-_DP_CRUISE_MIN_V_SPORT = [-0.70,  -0.80,  -0.90, -0.90,  -0.80, -0.70]
+_DP_CRUISE_MIN_V =       [-0.70,  -0.80,  -0.90, -0.90,  -0.80, -0.70]
+_DP_CRUISE_MIN_V_ECO =   [-0.65,  -0.60,  -0.73, -0.75,  -0.75, -0.75]
+_DP_CRUISE_MIN_V_SPORT = [-0.75,  -0.90,  -0.95, -0.95,  -0.85, -0.65]
 _DP_CRUISE_MIN_BP =      [0.,     0.07,   10.,   20.,    30.,   55.]
 
-_DP_CRUISE_MAX_V =       [3.5, 3.4, 2.1, 1.6, 1.1, 0.91, 0.68, 0.44, 0.34, 0.13]
-_DP_CRUISE_MAX_V_ECO =   [3.0, 1.7, 1.3, 0.7, 0.6, 0.44, 0.32, 0.22, 0.16, 0.0078]
-_DP_CRUISE_MAX_V_SPORT = [3.5, 3.5, 3.4, 3.0, 2.1, 1.61, 1.1,  0.63, 0.50, 0.33]
+_DP_CRUISE_MAX_V =       [3.5, 3.5, 3.4, 3.0, 2.1, 1.61, 1.1,  0.63, 0.50, 0.33]
+_DP_CRUISE_MAX_V_ECO =   [3.5, 3.4, 2.1, 1.6, 1.1, 0.91, 0.68, 0.44, 0.34, 0.13]
+_DP_CRUISE_MAX_V_SPORT = [3.5, 3.5, 4.1, 3.7, 2.6, 1.96, 1.31,  0.74, 0.58, 0.43]
 _DP_CRUISE_MAX_BP =      [0.,  3,   6.,  8.,  11., 15.,  20.,  25.,  30.,  55.]
 
 # d-e2e, from modeldata.h
@@ -100,9 +100,9 @@ def limit_accel_in_turns(v_ego, angle_steers, a_target, CP):
 class LongitudinalPlanner:
   def __init__(self, CP, init_v=0.0, init_a=0.0):
     # dp
-    self.dp_accel_profile_ctrl = False
-    self.dp_accel_profile = DP_ACCEL_ECO
-    self.dp_following_profile_ctrl = False
+    self.dp_accel_profile_ctrl = True
+    self.dp_accel_profile = DP_ACCEL_SPORT
+    self.dp_following_profile_ctrl = True
     self.dp_following_profile = 0
     self.cruise_source = 'cruise'
     self.vision_turn_controller = VisionTurnController(CP)
